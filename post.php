b090c144cfe1b8e99cb78e8ee8ce63e5495452be
Charles Lambret <79523370+CharlesLambret@users.noreply.github.com>
2022-05-19 23:37:23 +0200

    Version bugguée d'upload d'image
    Trois erreurs : Warning: Undefined array key "image" in C:\MAMP\htdocs\UglyAnimal\post.php on line 11
    
    Warning: Trying to access array offset on value of type null in C:\MAMP\htdocs\UglyAnimal\post.php on line 11
    
    Fatal error: Uncaught Error: Call to undefined function strgtolower() in C:\MAMP\htdocs\UglyAnimal\post.php:12 Stack trace: #0 {main} thrown in C:\MAMP\htdocs\UglyAnimal\post.php on line 12

--- post.php
+++ post.php
@@ -1,31 +1,50 @@
 <?php
-include('./Config/db.php');
-$resp = $db->query('SELECT * FROM Animals');
-
+ include('Config/db.php');
+ $resp = $db->query('SELECT * FROM Animals');
 $errors= '';
-if ($_POST &&
-isset($_POST['name']) && $_POST['name'] !== '' &&
-isset($_POST['summary']) && $_POST['summary'] !== '' &&
-isset($_POST['image']) && $_POST['image'] !== ''  &&{
-    $user = 'root';
-    $pass = 'root';
-    try {
-        $db, $user, $pass);
-    } catch (PDOException $e) {
-        print "Erreur!:" . $e->getMessage() . "<br/>";
-        die();
-    }
-    $req= $db->prepare("INSERT INTO animals(name, summary, image) VALUES (:name, :summary, :price, :weight)");
-    $req->execute([
-        'name' => $_POST['name'],
-        'summary' => $_POST['summary'],
-        'price' => $_POST['price'],
-        'weight' => $_POST['weight']
+if (
+    $_POST &&
+    isset($_POST['name']) && $_POST['name'] !== '' &&
+    isset($_POST['summary']) && $_POST['summary'] !== '' 
+  ) {
+
+$fileName= $_FILES['image']['name'];
+$fileExt = "." . strgtolower(substr(strrchr($fileName, "."), 1));
+
+$tmpName = $_FILES['image']['tmp_name'];
+$filePath= "img/" . $fileName . $fileExt;
+$req= $db->prepare("INSERT INTO animals(name, summary, image) VALUES (:name, :summary, :image,)");
+$req->execute([
+    'name' => $_POST['name'],
+    'summary' => $_POST['summary'],
+    'image' => $_POST['image'],
     ]);
     header('Location : index.php');
     exit;
 }
 
+$fileSize = $_FILES['image']['size'];
+$maxsize = 50000;
+$validExt = array('.jpg', '.png', '.gif', '.jpeg');
+
+
+$errors = '';
+    if (isset($_POST['name']) && $_POST['name'] === '') {
+      $errors .= 'Veuillez rentrer un nom ';
+    }
+
+    if (isset($_POST['summary']) && $_POST['summary'] === '') {
+      $errors .= 'Veuillez rentrer une description. ';
+    }
+    if (isset($_POST['image']) && $_POST['image'] === '') {
+        $errors .= 'Une image est requise. ';
+      }
+    if(!in_array($fileExt, $validExt)){
+          $errors.= 'Le format de fichier n est pas valide';
+      }
+      if($fileSize > $maxsize){
+          $errors.= 'La taille du fichier est trop volumineuse';
+      }
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -34,32 +53,24 @@ isset($_POST['image']) && $_POST['image'] !== ''  &&{
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<link href="./public/css/style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Exo:wght@500&family=Roboto+Flex:opsz@8..144&display=swap" rel="stylesheet"> 
 </head>
 
 <body>
-<nav class="navbar navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">Akueil</a>
-    <a class="navbar-brand" href="tablesmultiplications.php">Révise tes multiplications </a>
-    <a class="navbar-brand" href="calculatrice.php">Calculatrice</a>
-    <a class="navbar-brand" href="tableau.php">Tableau dynamique</a>
-    <a class="navbar-brand" href="nombres.php">Apprends à compter jusqu'à 100</a>
-    <a class="navbar-brand" href="nombresimpairs.php">Les nombres impairs jusqu'à 100</a>
-    <a class="navbar-brand" href="db.php">Database céréales</a>
-    <a class="navbar-brand" href="postcereale.php">Ajouter</a>
-  </div>
+<nav class="navbar fixed-top">
+    <a href="index.php"><img src="./public/img/Ugly Animals.png"></a>
 </nav>
     <?php 
         $user = 'root';
         $pass = 'root';
     ?>
-    <h1>Ajouter un céréale</h1>
+<h1>Ajouter un animal moche</h1>
 <form action ="" method="POST">
     Nom : <input type="text" name="name"/><br/>
-    Catégorie : <input type="text" name="category"/><br/>
-    Prix : <input type="text" name="price"/><br/>
-    Poids en kg : <input type="number" name="weight"/><br/>
+    Description : <input type="text" name="summary"/><br/>
+    Image : <input type="file" name="image"/><br/>
     <?php echo $errors ?>
     <input type="submit" value="Envoyer">
 </form>
