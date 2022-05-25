b1a5cff476dc178f7cc39d433bcba9b3940bd0d7
Charles Lambret <charleslambretpro@gmail.com>
2022-05-25 02:15:25 +0200

    Ajout du delete (fonctionnel) et de l'update(buggué)
    , rajout de détails graphiques

--- post.php
+++ post.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <?php
  include('Config/db.php');
  include('token.php');
@@ -74,3 +75,82 @@ $errors = '';
     <?php echo $errors ?>
     <input type="submit" value="Envoyer">
 </form>
+=======
+<?php
+ include('Config/db.php');
+ include('template/nav.php');
+ $resp = $db->query('SELECT * FROM Animals');
+ 
+$errors= '';
+if (
+    $_POST &&
+    isset($_POST['name']) && $_POST['name'] !== '' &&
+    isset($_POST['summary']) && $_POST['summary'] !== '' &&
+    isset($_POST['Type']) && $_POST['Type'] !== '' &&
+    isset($_POST['hp']) && $_POST['hp'] !== '' &&
+    isset($_POST['pc']) && $_POST['pc'] !== '' 
+    
+  ) {
+    $req= $db->prepare("INSERT INTO animals(name, summary, Type, pc, hp, image) VALUES (:name, :summary, :Type, :pc, :hp, :image)");
+    $req->execute([
+    'name' => $_POST['name'],
+    'summary' => $_POST['summary'],
+    'Type' => $_POST['Type'],
+    'pc' => $_POST['pc'],
+    'hp' => $_POST['hp'],
+    'image' => "public/img/pokeballmoche.jpg"
+    ]);
+    header('Location : index.php');
+    exit;
+}
+
+$errors = '';
+    if (isset($_POST['name']) && $_POST['name'] === '') {
+      $errors .= 'Veuillez rentrer un nom ';
+    }
+
+    if (isset($_POST['summary']) && $_POST['summary'] === '') {
+      $errors .= 'Veuillez rentrer une description. ';
+    }
+    
+    if(isset($_POST['Type']) && $_POST['Type'] === ''){
+      $errors .= 'Veuillez rentrer un type. ';
+    }
+
+    if(isset($_POST['hp']) && $_POST['hp'] === ''){
+      $errors .= 'Veuillez rentrer un hp. ';
+    }
+
+    if(isset($_POST['pc']) && $_POST['pc'] === ''){
+      $errors .= 'Veuillez rentrer un pc. ';
+    }
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="./public/css/style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Exo:wght@500&family=Roboto+Flex:opsz@8..144&display=swap" rel="stylesheet"> 
+</head>
+
+<body>
+<div style="width:80vw;margin-left:10vw;align-items:center;justify-content:center;margin-top:5vh;" class="d-flex flex-column">
+  <h1>Ajouter un animal moche</h1>
+  <form action="" method="POST" style="width:50vw;margin-left:25vw;">
+      Nom : <input type="text" name="name"/>
+      Description : <input type="text" name="summary"/><br/>
+      Type : <input type="text" name="Type"/><br/>
+      Point de combat : <input type="text" name="pc"/>
+      Point de vie : <input type="text" name="hp"/>
+      <?php echo $errors ?>
+      <input type="submit" class="btn btn-primary" value="Envoyer">
+  </form>
+</div>
+
+  </body>
+</html>
+>>>>>>> Stashed changes
--- template/nav.php
+++ template/nav.php
@@ -1,3 +1,12 @@
+<<<<<<< Updated upstream
 <nav class="navbar">
     <a href="index.php"><img src="./public/img/Ugly Animals.png"></a>
+=======
+<nav class="navbar">
+    <a href="index.php"><img src="./public/img/Ugly Animals.png"></a>
+    <div class="d-flex" style="justify-content:space-around;width:40vw;">
+        <a href="types.php">Liste des types d'animaux</a>
+        <a href="post.php">Ajouter un animal</a>
+    </div>
+>>>>>>> Stashed changes
     </nav>
